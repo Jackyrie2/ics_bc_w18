dba3762b625513683ae7b621c58d13b62bcee6ae
Jackyrie2 <jack0218wang@berkeley.edu>
2018-12-26 17:55:53 -0800

    Complete Chapter05 HW

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -4,3 +4,8 @@
 
 
 ### Your Code Here ###
+puts 'Hello, what\'s your favorite number'
+favorite_number = gets.chomp
+puts 'Wow, ' + favorite_number + ' is a cool number!'
+better_number = favorite_number.to_i + 1
+puts 'But I think ' +  better_number.to_s + ' is a even better number!'
--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -4,3 +4,10 @@
 
 
 ### Your Code Here ###
+puts 'Hello, what is your first name?'
+first_name = gets.chomp
+puts 'Hi ' + first_name + ', what is your last name?'
+last_name = gets.chomp
+puts 'Finally, what is your middle name?'
+middle_name = gets.chomp
+puts 'Hi ' + first_name + ' ' + middle_name + ' ' + last_name + ', it is very nice to meet you!'
