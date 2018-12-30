efdd508ba703a341ef86660bf1eb888f9c4a628e
Jackyrie2 <jack0218wang@berkeley.edu>
2018-12-29 20:18:23 -0800

    modified table of contents

--- week1/ch06/table_of_contents.rb
+++ week1/ch06/table_of_contents.rb
@@ -4,25 +4,15 @@
 ### Your Code Here ###
 line_width = 72
 
-ch01 = 'Chapter1:'
-ch02 = 'Chapter2:'
-ch03 = 'Chapter3:'
-ch04 = 'Chapter4:'
-ch05 = 'Chapter5:'
-ch06 = 'Chapter6:'
-ch07 = 'Chapter7:'
-ch08 = 'Chapter8:'
-ch09 = 'Chapter9:'
-
-t1 = 'Getting Started'
-t2 = 'Numbers'
-t3 = 'Letters'
-t4 = 'Variables and Assignment'
-t5 = 'Mixing It Up'
-t6 = 'More About Methods'
-t7 = 'Flow Control'
-t8 = 'Array and Iterators'
-t9 = 'Writing Your Own Methods'
+ch01 = 'Chapter1: Getting Started'
+ch02 = 'Chapter2: Numbers'
+ch03 = 'Chapter3: Letters'
+ch04 = 'Chapter4: Variables and Assignment'
+ch05 = 'Chapter5: Mixing It Up'
+ch06 = 'Chapter6: More About Methods'
+ch07 = 'Chapter7: Flow Control'
+ch08 = 'Chapter8: Array and Iterators'
+ch09 = 'Chapter9: Writing Your Own Methods'
 
 toc ='Table of Contents'
 p1 = 'page  1'
@@ -37,12 +27,12 @@ p9 = 'page 57'
 
 
 puts (toc.center(line_width))
-puts (ch01.ljust(line_width/3) + t1.center(line_width/3) + p1.rjust(line_width/3))
-puts (ch02.ljust(line_width/3) + t2.center(line_width/3) + p2.rjust(line_width/3))
-puts (ch03.ljust(line_width/3) + t3.center(line_width/3) + p3.rjust(line_width/3))
-puts (ch04.ljust(line_width/3) + t4.center(line_width/3) + p4.rjust(line_width/3))
-puts (ch05.ljust(line_width/3) + t5.center(line_width/3) + p5.rjust(line_width/3))
-puts (ch06.ljust(line_width/3) + t6.center(line_width/3) + p6.rjust(line_width/3))
-puts (ch07.ljust(line_width/3) + t7.center(line_width/3) + p7.rjust(line_width/3))
-puts (ch08.ljust(line_width/3) + t8.center(line_width/3) + p8.rjust(line_width/3))
-puts (ch09.ljust(line_width/3) + t9.center(line_width/3) + p9.rjust(line_width/3))
+puts (ch01.ljust(line_width/2) + p1.rjust(line_width/2))
+puts (ch02.ljust(line_width/2) + p2.rjust(line_width/2))
+puts (ch03.ljust(line_width/2) + p3.rjust(line_width/2))
+puts (ch04.ljust(line_width/2) + p4.rjust(line_width/2))
+puts (ch05.ljust(line_width/2) + p5.rjust(line_width/2))
+puts (ch06.ljust(line_width/2) + p6.rjust(line_width/2))
+puts (ch07.ljust(line_width/2) + p7.rjust(line_width/2))
+puts (ch08.ljust(line_width/2) + p8.rjust(line_width/2))
+puts (ch09.ljust(line_width/2) + p9.rjust(line_width/2))
