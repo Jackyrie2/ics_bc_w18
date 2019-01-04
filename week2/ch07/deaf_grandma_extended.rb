174b5505ed6b7043723c74ed556f5de88bb9b6b6
Jackyrie2 <jack0218wang@berkeley.edu>
2019-01-03 21:32:50 -0800

    assignment finished

--- week2/ch07/deaf_grandma_extended.rb
+++ week2/ch07/deaf_grandma_extended.rb
@@ -1,8 +1,26 @@
 puts 'COME GIVE GRANNY A KISS!'
 
 bye_counter = 0
-while () # Fill in the while condition
-  ### Your Code Here ###
+
+while true
+
+  input = gets.chomp
+
+  if input == 'BYE'
+    bye_counter = bye_counter + 1
+  else
+    bye_counter = 0
+  end
+
+  if bye_counter == 3
+    break
+  end
+
+  if input == input.upcase
+    puts 'NO, NOT SINCE 1938'
+  else puts 'HUH?! SPEAK UP, SONNY!'
+  end
+
 end
 
 puts 'OK SONNY, TALK AGAIN SOON'
