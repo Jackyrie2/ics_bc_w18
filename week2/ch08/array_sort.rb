df90ffb66efa6e4feed01136b0121e32376d29c8
Jackyrie2 <jack0218wang@berkeley.edu>
2019-01-04 17:54:34 -0800

    ch08 hw completegit add week2/ch08/table_of_contents.rb! finally XD

--- week2/ch08/array_sort.rb
+++ week2/ch08/array_sort.rb
@@ -3,8 +3,17 @@ word_list = []
 puts 'Please enter the desired words (one per line), and press enter when finished:'
 
 # Fetch the words, then output the sorted list
-while ()
+input = gets.chomp
+word_list.push input
+
+while input != ''
+
+  input = gets.chomp
+  input = input.downcase
+  word_list.push input
 
 end
 
-puts word_list # Make sure its sorted!
+word_list.pop
+
+puts word_list.sort # Make sure its sorted!
--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,9 +1,14 @@
 # Make sure you read the whole tip section, there's some good stuff there!
 
-line_width = # Set it to a default length you'll use throughout
+line_width = 72 # Set it to a default length you'll use throughout
 title = "Table of Contents"
 
 # Depending on your solution, you may use more than one array, but here's one to get you started
-chapters = [] 
-
+chapters = [1,2,3,4,5,6,7,8,9]
+chapter_name = ['Getting Started','Numbers','Letters','Variables and Assignment','Mixing It Up','More About Methods','Flow Control','Arrays and Iterators','Writing Your Own Methods']
+page = [1,9,13,17,21,27,37,51,57]
 ### Your Code Here ###
+puts title.center
+chapters.each do |chp|
+  puts 'Chapter' + chp + ':'
+end 
